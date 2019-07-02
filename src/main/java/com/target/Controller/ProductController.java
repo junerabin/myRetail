301fdd87e3cc51775f5f8488fa6c0c1e1ce22241
June Rabin <jrabin@umn.edu>
2019-07-02 02:17:04 -0500

    Add unit testing

--- src/main/java/com/target/Controller/ProductController.java
+++ src/main/java/com/target/Controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.target.Controller;
 
 import com.target.Exception.ResourceNotFoundException;
+import com.target.Model.Price;
 import com.target.Model.Product;
 import com.target.Service.PriceService;
 import com.target.Service.ProductService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/")
@@ -17,20 +19,28 @@ public class ProductController {
     private Product product;
 
     @Autowired
-    public ProductController(ProductService productService, PriceService priceService, Product product) {
+    private void setProductService(ProductService productService) {
         this.productService = productService;
+    }
+
+    @Autowired
+    private void setPriceService(PriceService priceService) {
         this.priceService = priceService;
+    }
+
+    @Autowired
+    private void setProduct(Product product) {
         this.product = product;
     }
 
     @RequestMapping(value="product/{id}",method=RequestMethod.GET)
-    public Product getProductById(@PathVariable int id) {
+    public Product getProductById(@PathVariable int id) throws URISyntaxException {
         Product productDetails = productService.findProductById(id, product);
         return productDetails;
     }
 
     @RequestMapping(value="product/{id}",method=RequestMethod.PUT)
-    public Product updateProduct(@PathVariable Integer id, @RequestBody Product updatedProductPrice) throws IOException {
+    public Product updateProduct(@PathVariable Integer id, @RequestBody Product updatedProductPrice) throws URISyntaxException {
         priceService.updateProductById(id, updatedProductPrice);
 
         return productService.findProductById(id,updatedProductPrice);
